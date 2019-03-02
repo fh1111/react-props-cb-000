b43b073b2c504114da7c8015f868d62e30a731ef
Fer Hernandez <fh.1111@gmail.com>
2019-03-02 16:23:35 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -19,7 +19,13 @@ class MovieCard extends React.Component {
   }
 }
 
+const jurassicWorldGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller'];
+ 
 ReactDOM.render(
-  <MovieCard />,
+  <MovieCard
+    title="Jurassic World"
+    poster="http://image.tmdb.org/t/p/w342/jjBgi2r5cRt36xF6iNUEhzscEcb.jpg"
+    genres={jurassicWorldGenres} 
+  />,
   document.getElementById('root')
 );
